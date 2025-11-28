43873ec53647a594a951ac14e818af2cb9c63463
Regan Cannell <regancannell@Regans-MacBook-Pro.local>
2025-11-28 18:35:15 +0200

    Added simple count example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,50 @@ import React, { useState } from 'react';
 
 function App() {
   const [isActive, setIsActive] = useState(false);
-  const buttonText = isActive ? 'Active' : 'Inactive';
+
+  // This is more concise
+  // const buttonText = isActive ? 'Active' : 'Inactive';
+
+  let buttonText = "";
+
+  if (isActive) {
+    buttonText = 'Active';
+  } else {
+    buttonText = 'Inactive';
+  }
+
+  const handleIsActiveOnClick = () => {
+    setIsActive(!isActive)
+  };
+
+  const [count, setCount] = useState(0);
+
+  const handleIncreaseCountOnClick = () => {
+    console.log("Current count: " + count);
+    setCount(count + 1);
+  }
+
+  const handleDecreaseCountOnClick = () => {
+    console.log("Current count: " + count);
+    setCount(count - 1);
+  }
+
+  console.log("Updated count: " + count);
 
   return (
-    <button onClick={() => setIsActive(!isActive)}>
-      { buttonText }
-    </button>
+    <>
+      <button onClick={handleIsActiveOnClick}>
+        { buttonText }
+      </button>
+
+      <button onClick={handleIncreaseCountOnClick}>
+        Increase Count
+      </button>
+      <button onClick={handleDecreaseCountOnClick}>
+        Decrease Count
+      </button>
+      <h1>{count}</h1>
+    </>
   );
 }
 
