42549e68dd5052c47e3762f19331a2c6fd9d1f11
Regan Cannell <regancannell@Regans-MacBook-Pro.local>
2025-11-28 16:19:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+  const buttonText = isActive ? 'Active' : 'Inactive';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => setIsActive(!isActive)}>
+      { buttonText }
+    </button>
   );
 }
 
